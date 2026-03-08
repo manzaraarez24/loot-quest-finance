20b8012fb0524b6aadf96060b6d458088b6437ad
Aarez Manzar <manzaraarez24@gmail.com>
2026-03-08 23:32:47 +0530

    Setup Capacitor for Android and GitHub Actions APK build

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,11 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'app.lovable.831a3797a1404a8aa517c8c30fdb2135',
   appName: 'LootBag',
-  webDir: 'dist',
-  server: {
-    url: 'https://831a3797-a140-4a8a-a517-c8c30fdb2135.lovableproject.com?forceHideBadge=true',
-    cleartext: true
-  }
+  webDir: 'dist'
 };
 
 export default config;
